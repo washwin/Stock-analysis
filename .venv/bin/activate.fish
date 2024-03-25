f8ad7270795575c5496865c8b7b461390d94beb4
washwin <210010060@iitdh.ac.in>
2024-03-25 21:21:48 +0530

    trying PMDARIMA

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/user/stock-analysis/.venv"
+set -gx VIRTUAL_ENV "/home/ashwin/Desktop/Stock-analysis/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
